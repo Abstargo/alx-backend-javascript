38d16715c712e2ea01f7f781b7563e9383dc96b1
Abstargo <james.iharo@gmail.com>
2024-10-08 12:19:42 +0100

    for real

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,8 @@
 export default function getListStudentIds(students) {
-	let studentList = [];
-	if (!(students instanceof Array)) {
-		return studentList;
-	}
-	studentList = students.map((student) => student.id);
-	return studentList;
+  let studentList = [];
+  if (!(students instanceof Array)) {
+    return studentList;
+  }
+  studentList = students.map((student) => student.id);
+  return studentList;
 }
